fde9e774cf0ffc6ca672d3d6d6133b575ec6bc18
rushid123 <109076087+rushid123@users.noreply.github.com>
2022-09-23 16:19:11 +0530

    Add files via upload

---
+++ sample_dashboard/sales/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+import pandas as pd
+
+def index(request):
+    """ view function for sales app """
+
+    # read data
+
+    df = pd.read_csv("car_sales.csv")
+    rs = df.groupby("Engine size")["Sales in thousands"].agg("sum")
+    categories = list(rs.index)
+    values = list(rs.values)
+
+    table_content = df.to_html(index=None)
+
+
+    table_content = table_content.replace("<thead>", "<thead class='thead-dark'>")
+    table_content = table_content.replace('class="dataframe"', "class='table table-striped'")
+    table_content = table_content.replace('border="1"', "")
+
+    context = {"categories": categories, 'values': values, 'table_data': table_content}
+    return render(request, 'index.html', context=context)
